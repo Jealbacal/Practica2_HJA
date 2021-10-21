8909b5728905410f9f00038664210bb3397295c1
Jeal <jesusagustiniano@gmail.com>
2021-10-21 22:42:12 +0200

    hice commit sin haber guardado el archivo xD

--- Practica2-HJA/src/main/java/poker/practica2/hja/ap1/Ap2.java
+++ Practica2-HJA/src/main/java/poker/practica2/hja/ap1/Ap2.java
@@ -4,10 +4,41 @@
  */
 package poker.practica2.hja.ap1;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author jesus
  */
 public class Ap2 {
+    
+    ArrayList<String>rankings;
+    int percent;
+    ArrayList<String>result;
+    int _size=13;
+
+    public Ap2(ArrayList<String> rankings, int percent){
+        
+        this.rankings=rankings;
+        this.percent=percent;
+        result= new ArrayList<String>();
+        
+    }
+    
+    public void bestRanking(){
+        
+        int best = ((_size*_size)*percent)/100;
+        
+        String aux="";
+        
+        for(int i=0;i<best;i++){
+            
+            aux=this.rankings.get(i);
+            result.add(aux);
+        }
+        
+    }
+    
+    
     
 }
